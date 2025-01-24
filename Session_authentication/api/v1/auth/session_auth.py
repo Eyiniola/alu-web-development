63234f92942df4a4fe864d95d9effd4e73cb4382
Eyiniola Fagbemi <127391120+Eyiniola@users.noreply.github.com>
2025-01-24 16:22:44 +0000

    task

---
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+SessionAuth module
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    SessionAuth class for session-based authentication.
+    Currently empty, to be implemented in later steps.
+    """
+    pass
