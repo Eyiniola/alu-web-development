e0d3820a5baf6e2062d8dbf78a8b04315fcbd1a6
Eyiniola Fagbemi <127391120+Eyiniola@users.noreply.github.com>
2025-01-24 16:28:43 +0000

    task

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -7,8 +7,15 @@ from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
-    """
-    SessionAuth class for session-based authentication.
-    Currently empty, to be implemented in later steps.
-    """
-    pass
+        """ Session auth class"""
+
+        user_id_by_session_id = {}
+
+        def create_session(self, user_id: str = None) -> str:
+            """ create session
+            """
+            if user_id is None or type(user_id) != str:
+                return None
+            session_id = str(uuid.uuid4())
+            self.user_id_by_session_id[session_id] = user_id
+            return session_id
